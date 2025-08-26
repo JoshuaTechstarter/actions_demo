d868db33de902df34982080a90b5701287b41b90
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-08-26 16:19:23 +0200

    Update calc.yaml, integrate slack webhook

--- testing/mwst.test.js
+++ testing/mwst.test.js
@@ -5,15 +5,15 @@ test('mwst: 7% von 100 = 7', () => {
 });
 
 test('mwst: 19% von 200 = 38', () => {
-    expect(mwst(200, 0.19)).toBe(38);
+    expect(mwst(200)).toBe(38);
 });
 
 test('mwst: Betrag 0 -> Steuer 0', () => {
-    expect(mwst(0, 0.19)).toBe(0);
+    expect(mwst(0)).toBe(0);
 });
 
 test('brutto: 100 netto bei 19% -> 119 Brutto', () => {
-    expect(brutto(100, 0.19)).toBe(119);
+    expect(brutto(100)).toBe(119);
 });
 
 test('brutto: 50 netto bei 7% -> 53.5 Brutto', () => {
